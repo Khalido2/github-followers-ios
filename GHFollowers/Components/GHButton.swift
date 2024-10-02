da1c84e14f93be3cac4bc9f20dc7ee9f473e9b78
Khalid Olowe-Makorie <khalid.olowemakorie@vodafone.com>
2024-10-02 10:56:36 +0100

    Build button and textfield components

---
+++ GHFollowers/Components/GHButton.swift
@@ -0,0 +1,35 @@
+//
+//  GAButton.swift
+//  GitApp
+//
+//  Created by "Khalid Olowe-Makorie, Vodafone" on 02/10/2024.
+//
+
+import UIKit
+
+class GHButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        //Initialiser used by storyboard
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColour: UIColor, title: String){
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColour
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false //use auto layout
+    }
+}
